18e9899dd123269161c43da57c276f797a7cdd12
Shavix <mrmater121@gmail.com>
2024-10-21 22:53:46 +0200

    Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://shavix.com',
+  base: 'Shavixinio/shavix.com',
+})
